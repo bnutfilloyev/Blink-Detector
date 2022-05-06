a02fb52f7f0a289dd060c49b55c26f850769cce5
Bekhruz Nutfilloyev <yoshlikmedia@gmail.com>
2022-05-06 12:19:38 +0500

    new algorithm

--- app/api/routes/predictor.py
+++ app/api/routes/predictor.py
@@ -1,6 +1,9 @@
+from cgi import print_form
 import cv2
 import imutils
 import numpy as np
+from regex import F
+from services.eye_detector import eye_blink_detection
 
 from core.config import TOKEN, CHAT_ID
 from fastapi import APIRouter, File, HTTPException, UploadFile
@@ -8,7 +11,7 @@ from models.prediction import Detection
 from services.predict import MachineLearningModelHandlerScore as model
 
 from services import blink_detection
-from aiogram import Bot, types
+from aiogram import Bot
 
 
 router = APIRouter()
@@ -16,31 +19,58 @@ bot = Bot(token=TOKEN)
 
 EYE_AR_THRESH = 0.15
 
+# # get base64 image
+# @router.post("/detect", response_model=Detection ,name="image:get-data")
+# async def image(file: UploadFile = File(...)):
+#     try:
+#         contents = await file.read()
+#         # print(contents)
+#         nparr = np.fromstring(contents, np.uint8)
+#         frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+#         frame = imutils.resize(frame, width=700)
+#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+#         EAR = blink_detection.calculate_ear(frame, gray)
+
+#         await bot.send_photo(chat_id=CHAT_ID, photo=contents, caption=f"EAR: {EAR}")
+
+#         if EAR is not None :
+#             if EAR <= EYE_AR_THRESH:
+#                 return Detection(status=False)
+#             else:
+#                 return Detection(status=True)
+#         else:
+#             return Detection(status=False)
+
+
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=f"Exception: {e}")
+
 # get base64 image
-@router.post("/detect", response_model=Detection ,name="image:get-data")
+@router.post("/detect", response_model=Detection, name="image:get-data")
 async def image(file: UploadFile = File(...)):
     try:
         contents = await file.read()
         # print(contents)
         nparr = np.fromstring(contents, np.uint8)
         frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        print(frame.shape)
 
-        frame = imutils.resize(frame, width=700)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        result, pred_l, pred_r = eye_blink_detection(frame)
 
-        EAR = blink_detection.calculate_ear(frame, gray)
+        await bot.send_photo(chat_id=CHAT_ID,
+                             photo=contents,
+                             caption=f"<b>result</b>: <code>{result}, </code>\n\n<b>Left: </b> <code>{pred_l[0][0]}, </code>\n<b>Right: </b> <code>{pred_r[0][0]}</code>",
+                             parse_mode="HTML")
 
-        await bot.send_photo(chat_id=CHAT_ID, photo=contents, caption=f"EAR: {EAR}")
-
-        if EAR is not None :
-            if EAR <= EYE_AR_THRESH:
+        if result is not None:
+            if result == "Blink":
                 return Detection(status=False)
             else:
                 return Detection(status=True)
         else:
             return Detection(status=False)
-
-
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Exception: {e}")
