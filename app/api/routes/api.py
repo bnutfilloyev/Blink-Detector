e3d8d256b1eaa33664b442f6a2f262ea80bdf6b5
Bekhruz Nutfilloyev <yoshlikmedia@gmail.com>
2022-05-11 19:20:15 +0500

    clean api.py

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -1,6 +1,5 @@
-from fastapi import APIRouter
-
 from api.routes import predictor
+from fastapi import APIRouter
 
 router = APIRouter()
 router.include_router(predictor.router, tags=["predictor"], prefix="/v1")
